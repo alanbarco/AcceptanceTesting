b5548ba6ac8ce1d28629bcda2167497636e12f46
jjgilces <jjgilces@espol.edu.ec>
2023-08-01 21:01:02 -0500

    all requirements

--- features/dining_experience.feature
+++ features/dining_experience.feature
@@ -18,3 +18,11 @@ Feature: Dining Experience Manager
         Given the system has a meal "Pizza familiar"
         When the user orders a total quantity of "6" meals
         Then the system should apply a "10" percent discount to the total cost
+    Scenario: Calculate total cost for a specific meal
+        Given the system has a meal "Pizza familiar"
+        When the user orders "2" of the meal
+        Then the system should calculate the total cost as "40"
+    Scenario: User attempts to order more than the maximum quantity
+        Given the system has a meal "Pizza familiar"
+        When the user enters a total quantity "101" for the order
+        Then the system should not accept the total quantity and display an error message
--- features/steps/dining_experience_steps.py
+++ features/steps/dining_experience_steps.py
@@ -26,3 +26,37 @@ def step_then_system_should_not_accept_quantity_and_display_error(context):
         assert True
     else:
         assert False, f"System incorrectly accepted invalid quantity: {context.quantity}"
+@when(u'the user orders a total quantity of "{total_quantity}" meals')
+def step_when_user_orders_total_quantity_meals(context, total_quantity):
+    context.total_quantity = int(total_quantity)
+    context.client_order = {"Pizza familiar": context.total_quantity}
+
+@then(u'the system should apply a "{discount}" percent discount to the total cost')
+def step_then_system_should_apply_discount_to_total_cost(context, discount):
+    expected_discount = float(discount) / 100
+    base_total_cost = dining.calc_base_total_cost(context.client_order)
+    actual_discount = dining.calc_qty_discount(context.client_order, base_total_cost) / base_total_cost
+    assert actual_discount == expected_discount, f"Expected {expected_discount} but got {actual_discount}"
+@when(u'the user orders "{quantity}" of the meal')
+def step_when_user_orders_quantity_of_meal(context, quantity):
+    context.meal_quantity = int(quantity)
+
+@then(u'the system should calculate the total cost as "{total_cost}"')
+def step_then_system_should_calculate_total_cost(context, total_cost):
+    expected_total_cost = float(total_cost)
+    actual_total_cost = dining.calc_meal_total_cost("Pizza familiar", context.meal_quantity)
+    assert actual_total_cost == expected_total_cost, f"Expected {expected_total_cost} but got {actual_total_cost}"
+
+@when(u'the user enters a total quantity "{total_quantity}" for the order')
+def step_when_user_enters_total_quantity_for_order(context, total_quantity):
+    context.total_quantity = int(total_quantity)
+    context.client_order = {"Pizza familiar": context.total_quantity}
+
+@then(u'the system should not accept the total quantity and display an error message')
+def step_then_system_should_not_accept_total_quantity_and_display_error(context):
+    try:
+        dining.validate_amount(context.client_order)
+    except ValueError:
+        assert True
+    else:
+        assert False, f"System incorrectly accepted invalid total quantity: {context.total_quantity}"
