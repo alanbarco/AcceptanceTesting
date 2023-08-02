a12f9b559124745ed7c516e55809781158bde0e7
Danny Loor <danny_loor00@hotmail.com>
2023-08-01 22:43:54 -0500

    format main code

--- features/steps/todo_list_steps.py
+++ features/steps/todo_list_steps.py
@@ -1,18 +1,23 @@
 from behave import given, when, then
 from todo_list import TodoList, Task
 
+
 @given('the to-do list is empty')
 def step_given_empty_todo_list(context):
     context.todo_list = TodoList()
+
 
 @when('the user adds a task "{task_name}" with priority "{priority}"')
 def step_when_user_adds_task(context, task_name, priority):
     context.todo_list.add_task(task_name, priority)
 
+
 @then('the to-do list should contain "{task_name}" with priority "{priority}"')
 def step_then_todo_list_contains(context, task_name, priority):
-    tasks = [str(task) for task in context.todo_list.tasks if task.name == task_name and task.priority == priority]
+    tasks = [str(task) for task in context.todo_list.tasks if task.name ==
+             task_name and task.priority == priority]
     assert tasks
+
 
 @given('the to-do list contains tasks')
 def step_given_todo_list_with_tasks(context):
@@ -20,51 +25,67 @@ def step_given_todo_list_with_tasks(context):
     for row in context.table:
         context.todo_list.add_task(row['Task'], row['Priority'])
 
+
 @when('the user lists all tasks')
 def step_when_user_lists_all_tasks(context):
     context.listed_tasks = context.todo_list.list_tasks()
 
+
 @then('the output should contain')
 def step_then_output_should_contain(context):
     for row in context.table:
-        tasks = [str(task) for task in context.todo_list.tasks if task.name == row['Task'] and task.priority == row['Priority']]
+        tasks = [str(task) for task in context.todo_list.tasks if task.name ==
+                 row['Task'] and task.priority == row['Priority']]
         assert tasks
+
 
 @when('the user marks task "{task_name}" as completed')
 def step_when_user_marks_task_as_completed(context, task_name):
     context.todo_list.mark_task_completed(task_name)
 
+
 @then('the to-do list should show task "{task_name}" as completed')
 def step_then_todo_list_should_show_task_as_completed(context, task_name):
-    tasks = [str(task) for task in context.todo_list.tasks if task.name == task_name and task.status == 'Completed']
+    tasks = [str(task) for task in context.todo_list.tasks if task.name ==
+             task_name and task.status == 'Completed']
     assert tasks
+
 
 @when('the user deletes task "{task_name}"')
 def step_when_user_deletes_task(context, task_name):
-    task_index = next((index for index, task in enumerate(context.todo_list.tasks) if task.name == task_name), None)
+    task_index = next((index for index, task in enumerate(
+        context.todo_list.tasks) if task.name == task_name), None)
     if task_index is not None:
         context.todo_list.delete_task(task_index)
 
+
 @then('the to-do list should not contain "{task_name}"')
 def step_then_todo_list_does_not_contain(context, task_name):
-    tasks = [str(task) for task in context.todo_list.tasks if task.name == task_name]
+    tasks = [str(task)
+             for task in context.todo_list.tasks if task.name == task_name]
     assert not tasks
+
 
 @when('the user edits the name of task "{task_name}" to "{new_task_name}"')
 def step_when_user_edits_task_name(context, task_name, new_task_name):
-    task = next((task for task in context.todo_list.tasks if task.name == task_name), None)
+    task = next(
+        (task for task in context.todo_list.tasks if task.name == task_name), None)
     if task:
         task.edit_name(new_task_name)
 
+
 @when('the user edits the priority of task "{task_name}" to "{new_priority}"')
 def step_when_user_edits_task_priority(context, task_name, new_priority):
-    task = next((task for task in context.todo_list.tasks if task.name == task_name), None)
+    task = next(
+        (task for task in context.todo_list.tasks if task.name == task_name), None)
     if task:
         task.edit_priority(new_priority)
+
 
 @when('the user clears the to-do list')
 def step_when_user_clears_todo_list(context):
     context.todo_list.clear_tasks()
+
 
 @then('the to-do list should be empty')
 def step_then_todo_list_is_empty(context):
--- todo_list.py
+++ todo_list.py
@@ -1,18 +1,20 @@
 # todo_list.py
 from datetime import date
+
+
 class Task:
     def __init__(self, name, priority, status='Pending', date=date.today()):
         self.name = name
         self.priority = priority
         self.status = status
         self.date = date
-        
+
     def mark_completed(self):
         self.status = 'Completed'
-    
+
     def edit_name(self, name):
         self.name = name
-    
+
     def edit_priority(self, priority):
         self.priority = priority
 
@@ -25,21 +27,21 @@ class TodoList:
         self.tasks = []
 
     def add_task(self, task_name, task_priority):
-        self.tasks.append(Task(task_name,task_priority))
+        self.tasks.append(Task(task_name, task_priority))
 
     def list_tasks(self):
-        i=0
+        i = 0
         print("#.TASKE NAME - STATUS - DATE - PRIORITY")
         for task in self.tasks:
             print(str(i)+"."+str(task))
-            i=i+1
+            i = i+1
 
     def mark_task_completed(self, task_name):
         for task in self.tasks:
             if task.name == task_name:
                 task.mark_completed()
                 break
-    
+
     def edit_task(self):
         print("1: Edit task name")
         print("2: Edit task priority")
@@ -52,12 +54,13 @@ class TodoList:
             task.edit_priority(task_priority)
         else:
             print("Invalid option. Please try again.")
-    
+
     def delete_task(self, task_number):
         del self.tasks[task_number]
 
     def clear_tasks(self):
         self.tasks = []
+
 
 # main.py
 if __name__ == "__main__":
@@ -73,22 +76,22 @@ if __name__ == "__main__":
         print("7: Exit")
 
         option = input("Choose an option: ")
-        
+
         if option == "1":
             task_name = input("Enter task name: ")
             task_priority = input("Enter task priority: ")
-            todo_list.add_task(task_name,task_priority)
+            todo_list.add_task(task_name, task_priority)
 
         elif option == "2":
             todo_list.list_tasks()
-            
+
         elif option == "3":
             task_name = input("Enter task name: ")
             todo_list.mark_task_completed(task_name)
 
         elif option == "4":
             todo_list.clear_tasks()
-        
+
         elif option == "5":
             todo_list.list_tasks()
             task_number = int(input("Enter task number to delete: "))
